741775efc9bd402913e37a9ce73e2b7c5cb4e5f0
Temitope Oyeniyi <temitope1234.to@gmail.com>
2023-05-04 23:39:25 -0400

    added changes

--- scripts/bash/git-cmds.sh
+++ scripts/bash/git-cmds.sh
@@ -6,3 +6,11 @@ git pull
 
 # to create a new branch from an existing branch
 git checkout -b <new-branch> <existing-branch>
+
+# when changes are made locally,
+1. add the file to the staging area
+  - git add .
+2. commit the changes to the local git repo
+  - git commit -m "added git-cmds file"
+3. push the changes to the remote repo
+  - git push -u origin temi
