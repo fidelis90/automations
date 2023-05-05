ac5fc6af911b78c685f5530a1dc4062598f523a7
Temitope Oyeniyi <temitope@MACUSSTG2524942.local>
2023-05-04 23:05:08 -0400

    added git-cmd.sh script

---
+++ scripts/bash/git-cmds.sh
@@ -0,0 +1,8 @@
+# check the status of the git repo
+git status
+
+# pull latest changes from the remote repo
+git pull
+
+# to create a new branch from an existing branch
+git checkout -b <new-branch> <existing-branch>
